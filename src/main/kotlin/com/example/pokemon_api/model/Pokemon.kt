1f109bee44708fef07ccaf16aa9b2144bc23a556
GaashShmilovich <gaash.shm@gmail.com>
2025-04-28 12:52:11 +0200

    Changed Pokémon ID to use the Pokédex number

--- src/main/kotlin/com/example/pokemon_api/model/Pokemon.kt
+++ src/main/kotlin/com/example/pokemon_api/model/Pokemon.kt
@@ -1,12 +1,17 @@
 package com.example.pokemon_api.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder("id", "name", "type", "img")
 data class Pokemon(
-    val id: Int,
+    @JsonProperty("num") private val num: String,
     val name: String,
     val type: List<String>,
-    val num: String,
     val img: String
-    )
+) {
+    val id: Int
+        get() = num.toInt()
+}
