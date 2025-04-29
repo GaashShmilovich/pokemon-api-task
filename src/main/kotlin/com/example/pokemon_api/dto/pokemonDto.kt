64139b360052b18677ca90a575abaa97cbf3ab99
GaashShmilovich <gaash.shm@gmail.com>
2025-04-29 12:12:42 +0200

    decouple PokemonType serialization from domain model

--- src/main/kotlin/com/example/pokemon_api/dto/pokemonDto.kt
+++ src/main/kotlin/com/example/pokemon_api/dto/pokemonDto.kt
@@ -2,10 +2,12 @@ package com.example.pokemon_api.dto
 
 import com.example.pokemon_api.model.Pokemon
 import com.example.pokemon_api.model.PokemonType
+import com.example.pokemon_api.model.PokemonTypeDeserializer
 import com.fasterxml.jackson.annotation.JsonAlias
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import java.net.URL
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -14,6 +16,7 @@ data class PokemonDto(
     @JsonProperty("num") private val num: String,
     val name: String,
     @JsonAlias("type")
+    @JsonDeserialize(contentUsing = PokemonTypeDeserializer::class)
     val types: List<PokemonType>,
     val img: URL
 ) {
